53d53e257e4acacd0f303930040dfd9959af01ce
zane-zip <102574865+zane-zip@users.noreply.github.com>
2022-04-05 19:38:51 +0900

    Add files via upload

---
+++ src/main/zepl3/Zepl3.java
@@ -0,0 +1,24 @@
+package zepl3.mcmcmcne.zepl3;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Zepl3 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+        System.out.println("プラグインが開始しました");
+        getLogger().info("プラグインが開始しました");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+
+        System.out.println("プラグインが停止しました");
+        getLogger().info("プラグインが停止しました");
+
+    }
+}
